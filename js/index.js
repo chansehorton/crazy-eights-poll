8ce616ef9a534828e726c094f326737e11bcd6fd
CHANSE HORTON <chansehorton@yahoo.com>
2016-10-27 19:59:04 -0600

    get request working, post request not

--- js/index.js
+++ js/index.js
@@ -10,11 +10,35 @@ $( document ).ready(function() {
     optionsArray.push($(this).children().val());
   });
 
-  $.post('https://strawpoll.me/api/v2/polls', {
+
+  // $.getJSON('https://g-strawpoll.herokuapp.com/api/v2/polls/1')
+  //   .done(function(response) {
+  //     console.log(response);
+  //   })
+  //   .fail(function(response) {
+  //     console.log('error');
+  //     console.log(response);
+  //   });
+
+  console.log(JSON.stringify({
+    title: 'chanse test',
+    options: optionsArray,
+    multi: false
+  }));
+// {
+//    "title": "This is a test poll.",
+//    "options": [
+//        "Option #1",
+//        "Option #2"
+//    ],
+//    "multi": true
+// })
+
+  $.post('https://g-strawpoll.herokuapp.com/api/v2/polls', JSON.stringify({
     title: 'chanse test poll',
     options: optionsArray,
     multi: false
-  })
+  }))
     .done(function(response) {
       console.log(response);
     })
