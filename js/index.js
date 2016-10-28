35fc13f12785a9e136d74d522d555f5c093cf062
CHANSE HORTON <chansehorton@yahoo.com>
2016-10-28 15:44:41 -0600

    adding readme and wireframe screenshots

--- js/index.js
+++ js/index.js
@@ -11,34 +11,7 @@ $( document ).ready(function() {
   });
 
 
-  // $.getJSON('https://g-strawpoll.herokuapp.com/api/v2/polls/1')
-  //   .done(function(response) {
-  //     console.log(response);
-  //   })
-  //   .fail(function(response) {
-  //     console.log('error');
-  //     console.log(response);
-  //   });
-
-  console.log(JSON.stringify({
-    title: 'chanse test',
-    options: optionsArray,
-    multi: false
-  }));
-// {
-//    "title": "This is a test poll.",
-//    "options": [
-//        "Option #1",
-//        "Option #2"
-//    ],
-//    "multi": true
-// })
-
-  $.post('https://g-strawpoll.herokuapp.com/api/v2/polls', JSON.stringify({
-    title: 'chanse test poll',
-    options: optionsArray,
-    multi: false
-  }))
+  $.getJSON('http://api.bing.net/json.aspx?Appid=Ou2BjEPSIlmePepgIZPQ4sSBTRZTJhmDUrJDcHxsYv0=&query=sushi&sources=web&web.count=1')
     .done(function(response) {
       console.log(response);
     })
@@ -46,4 +19,6 @@ $( document ).ready(function() {
       console.log('error');
       console.log(response);
     });
+
+
 });
