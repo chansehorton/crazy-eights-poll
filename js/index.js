eed59e524f662fe2527feb20f150f2125ba29a62
CHANSE HORTON <chansehorton@yahoo.com>
2016-10-31 13:53:02 -0600

    main page dynamically generated, now working on buttons

--- js/index.js
+++ js/index.js
@@ -1,24 +1,81 @@
 'use strict';
 
+//'inputArray' is array of jQuery objects
+function filterFirstRound (inputArray) {
+  $.each(inputArray, function() {
+    var searchTerm = ($(this).children().val());
+    var urlSearchString = 'http://g-bing.herokuapp.com/bing/v5.0/search?q=' + searchTerm;
+
+    $.ajax({
+      url: urlSearchString,
+      headers: {
+          'Ocp-Apim-Subscription-Key': '7e03310d1c484161a53c9b85033536d3'
+      },
+      method: 'GET',
+      dataType: 'json',
+      data: {
+        count: '1',
+        mkt: 'en-us'
+      },
+      success: function(response){
+        console.log('Success: ');
+        console.log(response);
+      },
+      error: function(response) {
+        console.log('ERROR: ');
+        console.log(response);
+        alert('There was a problem with the search request.' + searchTerm);
+      }
+    });
+  });
+};
+
 $( document ).ready(function() {
-  $(".button-collapse").sideNav();
 
-  var pollItems = $('#poll_items').children().children();
-  var optionsArray = [];
-
-  $.each(pollItems, function() {
-    optionsArray.push($(this).children().val());
+  $.get("http://localhost:8080/round_1_layout.html", function(data){
+    console.log(data);
+    $(data).prependTo('#poll_items');
   });
 
+  var pollItems = $('#poll_items').children().children();
 
-  $.getJSON('http://api.bing.net/json.aspx?Appid=Ou2BjEPSIlmePepgIZPQ4sSBTRZTJhmDUrJDcHxsYv0=&query=sushi&sources=web&web.count=1')
-    .done(function(response) {
-      console.log(response);
-    })
-    .fail(function(response) {
-      console.log('error');
-      console.log(response);
-    });
-
-
+  $('#submit_button').click(function(e) {
+    e.preventDefault();
+    filterFirstRound(pollItems);
+  });
 });
+
+
+
+  // Text analysis ajax call:
+
+  // var urlTAString = '';
+  // var urlTAString = 'https://westus.api.cognitive.microsoft.com/text/analytics/v2.0/keyPhrases';
+
+  // $.ajax({
+  //   url: urlTAString,
+  //   headers: {
+  //     'Content-Type': 'application/json',
+  //     'Ocp-Apim-Subscription-Key': '358f38392fde4104ac79ff4b235a3c9d'
+  //   },
+  //   method: 'POST',
+  //   dataType: 'json',
+  //   data: JSON.stringify({
+  //     'documents':[
+  //       {
+  //         'id': '1',
+  //         'language': 'en',
+  //         'text': ''
+  //       }
+  //     ]
+  //   }),
+  //   success: function(data){
+  //     console.log('success: ');
+  //     console.log(data);
+  //   },
+  //   error: function(data) {
+  //     console.log('error: ');
+  //     console.log(this);
+  //     console.log(data);
+  //   }
+  // });
