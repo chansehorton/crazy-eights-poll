8eadab13995dc04e17261c092e62790b8b132d81
CHANSE HORTON <chansehorton@yahoo.com>
2016-11-01 14:49:28 -0600

    committing with refactorDisplay partially written. still needpromises for main functionality

--- js/index.js
+++ js/index.js
@@ -1,8 +1,10 @@
 'use strict';
 
 //'inputArray' is array of jQuery objects
-function filterFirstRound (inputArray) {
-  $.each(inputArray, function() {
+function filterFirstRound (inputJQOArray) {
+  var outputObjArray =[];
+  $.each(inputJQOArray, function() {
+    var searchReturn = {};
     var searchTerm = ($(this).children().val());
     var urlSearchString = 'http://g-bing.herokuapp.com/bing/v5.0/search?q=' + searchTerm;
 
@@ -18,31 +20,112 @@ function filterFirstRound (inputArray) {
         mkt: 'en-us'
       },
       success: function(response){
-        console.log('Success: ');
-        console.log(response);
+        searchReturn.term = searchTerm;
+        searchReturn.hits = response.webPages.totalEstimatedMatches;
+        outputObjArray.push(searchReturn);
       },
       error: function(response) {
-        console.log('ERROR: ');
-        console.log(response);
-        alert('There was a problem with the search request.' + searchTerm);
+        console.log('ERROR: ' + searchTerm);
       }
     });
   });
+  return outputObjArray;
 };
 
-$( document ).ready(function() {
 
-  $.get("http://localhost:8080/round_1_layout.html", function(data){
-    console.log(data);
-    $(data).prependTo('#poll_items');
-  });
+function compare(a,b) {
+  if (a.hits > b.hits)
+    {return -1;}
+  if (a.hits < b.hits)
+    {return 1;}
+  return 0;
+};
 
-  var pollItems = $('#poll_items').children().children();
+function refactorDisplay(array) {
+  $('#poll_items').children().children().not('.button').remove();
+
+  if (array.length>4) {
+    $('#poll_items').first().prepend('<div class="row top-row"></div><div class="row bottom-row"></div>');
+
+    for (var i=0;i<array.length;i++) {
+      if (i<2) {
+        $('.top-row').append('<div class="col l5 m5 s10 push-l1 push-m1 push-s1"><textarea class="poll-item-med" cols="30" rows="10" maxlength="300">' +  array[i].term + '</textarea></div>');
+      } else {
+        $('.bottom-row').append('<div class="col l5 m5 s10 push-l1 push-m1 push-s1"><textarea class="poll-item-med" cols="30" rows="10" maxlength="300">' +  array[i].term + '</textarea></div>');
+      };
+    };
+  } else {
+    $('#poll_items').first().prepend('<div class="row top-row">');
+  };
+}
+
+
+$(document).ready(function() {
+
+  var testArray1 = [
+    {term: 'crazy eights',
+     hits: 1},
+    {term: 'forward unto the breach',
+     hits: 2},
+    {term: 'carousel',
+     hits: 3},
+    {term: 'blue sock removal',
+     hits: 4},
+    {term: 'subjugated shoehorn',
+     hits: 5},
+    {term: 'yellow submarine',
+     hits: 6},
+    {term: 'carpet cleaning',
+     hits: 7},
+    {term: 'fallujah supply management',
+     hits: 8}
+  ];
 
   $('#submit_button').click(function(e) {
     e.preventDefault();
-    filterFirstRound(pollItems);
+    var pollItemsJQOBatch2 = $('#poll_items').children().children().not('.button');
+    var pollItemsJQOBatch1 = pollItemsJQOBatch2.splice(0,4);
+
+    // link to ajax search query function here
+
+    var testArray2 = [];
+
+    testArray1.sort(compare);
+    var outArray = testArray1.splice(0,4);
+
+
+    refactorDisplay(outArray);
+    // var pollItems = $('#poll_items').children().children().not('.button');
+    // var pollItemsJQOBatch1 = pollItems.slice(0,1);
+    // var pollItemsJQOBatch2 = pollItems.slice(1,2);
+
+
+    function getExample() {
+      var filteredResults1 = new Promise(function(resolve) {
+        return resolve(filterFirstRound(pollItemsJQOBatch1));
+      });
+      // var filteredResults2 = filteredResults1.then(function(data) {
+      //   return new Promise(function(resolve) {
+      //     resolve(filterFirstRound(pollItemsJQOBatch2));
+      //   });
+      //
+      //     // setTimeout(function(){
+      //     //   console.log("in set timeout");
+      //     // }, 1000);
+      //     // console.log("1st one",data);
+      //   });
+      // });
+
+      filteredResults2.then(function(data){
+        console.log(data);
+      });
+
+
+    };
+
+
   });
+
 });
 
 
---
+++ js/promise.js
@@ -0,0 +1,83 @@
+// function getExample() {
+//   var filteredResults1 = new Promise(function(resolve) {
+//     return filterFirstRound(pollItemsJQOBatch1);
+//   });
+//   filteredResults1.resolve('somejunk').then(function(data) {
+//     console.log(data);
+//   });
+//   var filteredResults2 = filteredResults1.then(function(resultA) {
+//     return new Promise(function(resolve) {
+//       setTimeout(function() {
+//         filterFirstRound(pollItemsJQOBatch2);
+//       }, 1000);
+//     });
+//   });
+//   Promise.all([filteredResults1, filteredResults2]).then(function(resultA, resultB) {
+//     console.log(resultA);
+//     console.log(resultB);
+//   });
+// };
+
+
+
+
+// Promise.all([filteredResults1, filteredResults2]).then(function(result1, result2){
+//   console.log("result1 in Promise.all",result1);
+//   return result1.concat(result2);
+// });
+
+// filteredResults2.then(function(data){
+//   console.log("in #2 then");
+//   console.log(data);
+// })
+// var filteredResults2 = filteredResults1.then(function(resultA) {
+//   return new Promise(function(resolve) {
+//     setTimeout(function() {
+//       filterFirstRound(pollItemsJQOBatch2);
+//     }, 1000);
+//   });
+// });
+// Promise.all([filteredResults1, filteredResults2]).then(function(resultA, resultB) {
+//   console.log(resultA);
+//   console.log(resultB);
+// });
+
+
+
+
+
+
+
+    // var allResults = getExample();
+    // console.log(allResults);
+
+
+
+    // var filteredResults1 = new Promise(function(resolve) {
+    //   filterFirstRound(pollItemsJQOBatch1);
+    // });
+    //
+    // filteredResults1.then(function(values) {
+    //   setTimeout(function() {
+    //     var filteredResults2 = new Promise(function(resolve) {
+    //       filterFirstRound(pollItemsJQOBatch2);
+    //     });
+    //   }, 1000);
+    // });
+    // var filteredResults1 = filterFirstRound(pollItemsJQOBatch1);
+    // var resultsProm2;
+    // var filteredResults2 = new Promise(function(resolve) {
+    //   window.setTimeout(function() {
+    //     resultsProm2 = filterFirstRound(pollItemsJQOBatch2);
+    //   }, 1000);
+    // });
+
+    // var resultsProm1 = Promise.resolve(filteredResults1);
+    // var resultsProm2 = Promise.resolve(filteredResults2);
+
+
+    // Promise.all([resultsProm1, resultsProm2]).then(function(values) {
+    //   console.log(values);
+    //   console.log(resultsProm1);
+    //   console.log(resultsProm2);
+    // });
